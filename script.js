2b702b7b81e7638c7d971658713470c874ac417d
Evgeniya <kolomievskayazhenya@mail.ru>
2023-04-04 22:15:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const circleIcons = document.querySelectorAll(".circle-icon");
-
+const girlSection = document.querySelector(".img-girl-section");
+const descrSection = document.querySelectorAll(" .content *:not(.main-text) ");
+const header = document.querySelectorAll(".main-text ");
+const signUpBtn = document.querySelector(".sign-up-btn");
+const burger = document.querySelector(".burger");
+if (window.innerWidth < 1024) {
+  document.querySelector(".logo__img").src = "source/Logo-white.svg";
+}
+console.log(descrSection);
 window.addEventListener("load", () => {
   circleIcons.forEach((icon) => {
     icon.classList.add("active");
@@ -9,7 +17,6 @@ window.addEventListener("load", () => {
 fetch("https://baconipsum.com/api/?type=lucky")
   .then((response) => response.json())
   .then((data) => {
-    console.log(typeof data);
     JSON.stringify(data[1]);
 
     const description = data[1].match(/^([\w]+\b[\W]*){1,14}/)[0]; // получаем первый элемент массива с описанием
@@ -17,3 +24,56 @@ fetch("https://baconipsum.com/api/?type=lucky")
     descriptionElem.textContent = description; // добавляем описание в элемент
   })
   .catch((error) => console.log(error));
+
+function changeImageSrc() {
+  if (window.innerWidth < 1024) {
+    document.querySelector(".logo__img").src = "source/Logo-white.svg";
+  } else {
+    document.querySelector(".logo__img").src = "source/Logo.svg";
+  }
+}
+window.addEventListener("resize", changeImageSrc);
+
+if (window.innerWidth < 650) {
+  signUpBtn.innerHTML =
+    '<img class="login" src="source/login.svg" alt="Login icon">';
+}
+
+function changeLogin() {
+  if (window.innerWidth < 650) {
+    signUpBtn.innerHTML =
+      '<img class="login" src="source/login.svg" alt="Login icon">';
+  } else {
+    signUpBtn.innerHTML = "Sign Up";
+  }
+}
+window.addEventListener("resize", changeLogin);
+
+const mobileAppear = (e) => {
+  girlSection.classList.add("disappear");
+  descrSection.forEach((element) => {
+    element.classList.add("appear");
+  });
+  header.forEach((element) => {
+    element.style.transition = "all 1s ease-out";
+    element.style.transform = "translateY(0)";
+  });
+  document.querySelector(".input-invalid").style.transform = "translateY(0)";
+  document.querySelector(".input-invalid").classList.remove("appear");
+};
+window.addEventListener("touchmove", mobileAppear);
+
+let nav_opened = false;
+
+function openNav() {
+  if (nav_opened == false) {
+    document.querySelector(".navigation ").classList.add("nav__active");
+    console.log(nav_opened);
+    nav_opened = true;
+  } else {
+    document.querySelector(".navigation ").classList.remove("nav__active");
+    nav_opened = false;
+  }
+}
+
+burger.addEventListener("click", openNav);
